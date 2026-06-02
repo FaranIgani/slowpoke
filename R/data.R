c665ea071d38ad623ae65264b0be36669a0a535a
Faran Igani <f95igani@gmail.com>
2026-06-01 19:50:10 -0700

    updated

--- R/data.R
+++ R/data.R
@@ -4,18 +4,15 @@
 #' @export
 .pokemon_cache <- new.env(parent = emptyenv())
 
+# only gets it once! next time you run or use load_data its already cached
 load_data <- function() {
   if (!exists("dat", envir = .pokemon_cache)) {
     path <- "https://www.dropbox.com/scl/fi/tnl4wcmgduu3bnmmllz2u/pokemon_cards.csv?rlkey=h7evg3hr4ckzqrxrzoy458ojs&st=uzrlktbc&dl=1"
-
-    .pokemon_cache$dat <- readr::read_csv(
-      path,
-      show_col_types = FALSE
-    )
+    .pokemon_cache$dat <- arrow::read_csv_arrow(path)
   }
-
   .pokemon_cache$dat
 }
+
 
 # load_data <- function() {
 #
